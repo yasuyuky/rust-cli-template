aca2b19941d8677d0c708161c38527d1bc6c0ecf
yasuyuky <yasuyuki.ymd@gmail.com>
2023-05-06 18:28:06 +0900

    Refactor printing of version information in main.rs to be more concise

--- src/main.rs
+++ src/main.rs
@@ -61,9 +61,7 @@ fn main() -> Result<()> {
     }
     match command {
         Command::Do { something } => unimplemented!("{}", something),
-        Command::Version => {
-            println!("{}", VERSION_INFO);
-        }
+        Command::Version => println!("{}", VERSION_INFO),
         Command::Completion { shell } => {
             let shell = match shell {
                 Shell::Bash => shells::Shell::Bash,
